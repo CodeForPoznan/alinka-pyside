2598f4b92aa9ccf9dd2969012ddc54245271627a
RafalKufel <rafal.kufel@gmail.com>
2023-06-10 11:23:17 +0200

    Change db session maker

--- app/db/connection.py
+++ app/db/connection.py
@@ -1,5 +1,3 @@
-from contextlib import contextmanager
-
 from config import settings
 from db.models import Base
 from sqlalchemy import create_engine
@@ -9,8 +7,4 @@ engine = create_engine(f"sqlite:///{settings.DB_PATH}")
 Base.metadata.create_all(engine)
 
 
-@contextmanager
-def db_session():
-    db_session = scoped_session(sessionmaker(autocommit=False, autoflush=True, bind=engine))
-    yield db_session
-    db_session.close()
+db_session = scoped_session(sessionmaker(bind=engine))
