8d09245a00853e5ffb26db6c37dcabdea2cb032b
= <=>
2025-05-07 19:16:32 +0200

    [66] Bump flake8

--- migrations/env.py
+++ migrations/env.py
@@ -9,7 +9,7 @@ from alinka.db.models import Base
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", f"sqlite:///{settings.DB_PATH}")  # noqa: E231
+config.set_main_option("sqlalchemy.url", f"sqlite:///{settings.DB_PATH}")
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
