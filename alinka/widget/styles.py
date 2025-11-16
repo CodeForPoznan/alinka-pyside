9697bb84e6a8d0270cc61528f1ce6e4c20d55b22
= <=>
2025-11-16 21:47:40 +0100

    [184] Make style

--- alinka/widget/styles.py
+++ alinka/widget/styles.py
@@ -244,7 +244,7 @@ def get_custom_overrides_stylesheet() -> str:
         color: {COLORS["text_primary"]} !important;
         background-color: {COLORS["background"]};
     }}
-   
+
     QComboBox QAbstractItemView::item:selected {{
         color: {COLORS["text_primary"]} !important;
         background-color: {COLORS["tab_hover"]} !important;
