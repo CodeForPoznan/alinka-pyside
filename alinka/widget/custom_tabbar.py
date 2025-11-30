a8441860eea1b53aa22042b7e9a4eb4c136b3965
kabix09 <kabix.009@gmail.com>
2025-11-30 16:27:29 +0100

    Increate tab margin for warning icon

--- alinka/widget/custom_tabbar.py
+++ alinka/widget/custom_tabbar.py
@@ -1,6 +1,6 @@
 """Custom QTabBar that supports validation highlighting."""
 
-from PySide6.QtCore import QPoint, QRect, Qt
+from PySide6.QtCore import QPoint, QRect, Qt, QSize
 from PySide6.QtGui import QBrush, QColor, QFont, QPainter
 from PySide6.QtWidgets import QTabBar
 
@@ -11,11 +11,24 @@ class ValidationTabBar(QTabBar):
     def __init__(self, parent=None):
         super().__init__(parent)
         self._invalid_tabs = []
+        self.icon_margin = 10  # margines od prawej krawędzi tab'a
+        self.warning_icon = "⚠"
+
 
     def set_invalid_tabs(self, invalid_indices: list[int]) -> None:
         """Set which tab indices should be painted as invalid."""
         self._invalid_tabs = invalid_indices
         self.update()
+
+    def tabSizeHint(self, index: int) -> QSize:
+        """Increase the width of the tab to accommodate the warning icon."""
+        size = super().tabSizeHint(index)
+        font_metrics = self.fontMetrics()
+        text_width = font_metrics.horizontalAdvance(self.tabText(index))
+        icon_width = font_metrics.horizontalAdvance(self.warning_icon)
+        extra_width = icon_width + 40 + self.icon_margin
+        size.setWidth(max(size.width(), text_width + extra_width))
+        return size
 
     def paintEvent(self, event):
         """Override paint to draw validation indicators for invalid tabs."""
@@ -48,8 +61,9 @@ class ValidationTabBar(QTabBar):
             painter.setPen(icon_color)
 
             font_metrics = painter.fontMetrics()
-            icon_x = tab_rect.right() - 20
+
+            icon_x = tab_rect.right() - self.icon_margin - font_metrics.horizontalAdvance(self.warning_icon)
             icon_y = tab_rect.center().y() + (font_metrics.ascent() // 2)
-            painter.drawText(QPoint(icon_x, icon_y), "⚠")
+            painter.drawText(QPoint(icon_x, icon_y), self.warning_icon)
 
             painter.restore()
