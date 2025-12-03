b35f1663984137f58f6c671ab1717485a5396d03
stanislawK <stanislaw.krupienko@gmail.com>
2025-12-04 00:10:47 +0100

    clear_application_btn style changes

--- alinka/widget/containers/main_body/footer/application.py
+++ alinka/widget/containers/main_body/footer/application.py
@@ -17,18 +17,16 @@ class ApplicationFooterContainer(QFrame):
 
         self.cancel_btn = QPushButton("Anuluj", self)
         self.cancel_btn.clicked.connect(self.cancel_application)
-        layout.addWidget(self.cancel_btn)
 
         layout.addStretch()
 
         self.print_btn = QPushButton("Drukuj dokumenty", self)
         self.print_btn.clicked.connect(self.print_documents)
         self.clear_application_btn = QPushButton("Wyczyść formularz", self)
-        self.clear_application_btn.setStyleSheet("background-color: red; color: white;")
+        layout.addWidget(self.print_btn, stretch=3)
         self.clear_application_btn.clicked.connect(self.clear_application)
         layout.addWidget(self.clear_application_btn, stretch=1)
-        self.print_btn.setFixedWidth(200)
-        layout.addWidget(self.print_btn, stretch=3)
+        layout.addWidget(self.cancel_btn)
 
         self.setVisible(visible)
 
@@ -88,4 +86,10 @@ class ApplicationFooterContainer(QFrame):
         self.redirect_to_browser()
 
     def clear_application(self):
+        if not ConfirmationModal(
+            self,
+            "Potwierdzenie usunięcia zmian",
+            "Czy na pewno chcesz usunąć wszystkie wprowadzone dane?",
+        ).confirm():
+            return
         self.content_container.application_container.clear()
